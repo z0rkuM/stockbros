b6c292d555c5ea771fb237319730341d12d40580
JohnnySo <juan.sonr@gmail.com>
2017-04-18 13:20:49 +0200

    get methods for stock indicators

--- StockIndicators/StockIndicators.py
+++ StockIndicators/StockIndicators.py
@@ -1,12 +1,28 @@
 #!flask/bin/python
 from flask import Blueprint, jsonify
+from Authentication.Authentication import *
+from util import *
 
 api_si = Blueprint('api_si', __name__)
 
-#TODO: probar a meter token de seguridad a ver se funciona
-@api_si.route("/stock_indicators")
+@api_si.route("/stock_indicators", methods=['GET'])
+@auth.login_required
 def get_stock_indicators():
-    return jsonify(stock_indicators=[
-        {"username": "alice", "user_id": 1},
-        {"username": "bob", "user_id": 2}
-    ])
+	documentSI = db.stock_indicators
+	sis = []
+	for stockIndicator in documentSI.find({}):
+		sis.append(stockIndicator)
+	
+	return jsonify({'stock_indicators': [make_public_stock_indicator(si) for si in sis]})
+	
+@api_si.route('/stock_indicators/<stock_id>', methods=['GET'])
+@auth.login_required
+def get_event(stock_id):
+	documentSI = db.stock_indicators
+
+	si = documentSI.find_one({'stock':stock_id})
+	
+	if not si:
+		abort(404)
+
+	return jsonify({'stock_indicators':[make_public_stock_indicator(si)]})

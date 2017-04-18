89ef349d4aea906b31d6460c3b7b6f31d502a1f3
JohnnySo <juan.sonr@gmail.com>
2017-04-18 13:19:55 +0200

    get methods for stock indicators

--- util.py
+++ util.py
@@ -10,3 +10,13 @@ def make_public_event(event):
 		else:
 			recent_event[field] = event[field]
 	return recent_event
+	
+def make_public_stock_indicator(si):
+	recent_si = {}
+	for field in si:
+		if field == 'stock':
+			recent_si[field] = si[field]
+			recent_si['uri'] = url_for('api_si.get_stock_indicators', stock_id=si['stock'], _external=True)
+		else:
+			recent_si[field] = si[field]
+	return recent_si

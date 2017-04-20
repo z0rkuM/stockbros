ecb4407abf1520b6f6e95388ae07aaa3254aa7e6
JohnnySo <juan.sonr@gmail.com>
2017-04-20 16:48:11 +0200

    añado un punto al método getEvent porque ahora es un blueprint

--- util.py
+++ util.py
@@ -6,7 +6,7 @@ def make_public_event(event):
 	for field in event:
 		if field == '_id':
 			recent_event[field] = event[field]
-			recent_event['uri'] = url_for('get_event', event_id=event['_id'], _external=True)
+			recent_event['uri'] = url_for('.get_event', event_id=event['_id'], _external=True)
 		else:
 			recent_event[field] = event[field]
 	return recent_event

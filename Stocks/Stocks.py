3c739527d68b5e05c53a5dfc94a632e1d4f28fe1
JhonnySo <juan.sonr@gmail.com>
2017-04-23 11:35:54 +0200

    metodo put para añadir o actualizar stocks

--- Stocks/Stocks.py
+++ Stocks/Stocks.py
@@ -1,5 +1,5 @@
 #!flask/bin/python
-from flask import Blueprint, jsonify, abort
+from flask import Blueprint, jsonify, abort, request, make_response
 from Authentication.Authentication import *
 from util import *
 
@@ -11,6 +11,7 @@ def get_stocks():
 	documentStocks = db.stocks
 	stocks = []
 	for stock in documentStocks.find({}):
+		stock.pop("_id")
 		stocks.append(stock)
 	return jsonify({'stocks': [make_public_stock(s) for s in stocks]})
 	
@@ -23,5 +24,26 @@ def get_stock(stock_id):
 	
 	if not stock:
 		abort(404)
+	
+	stock.pop("_id")
 
 	return jsonify({'stocks':[make_public_stock(stock)]})
+
+@api_stocks.route('/stocks', methods=['PUT'])
+@auth.login_required
+def create_or_update_stock():
+
+	if not request.json:
+		abort(400)
+		
+	stocksSize = len(request.json)
+	
+	documentStocks = db.stocks
+	matchedCount = 0
+	modifiedCount = 0
+	for stock in request.json:
+		result = documentStocks.replace_one({"$and":[{"market": {'$eq':stock['market']}},{"stock": {'$eq':stock['stock']}}]}, stock, True)
+		matchedCount = matchedCount + result.matched_count
+		modifiedCount = modifiedCount + result.modified_count
+
+	return make_response(jsonify({'stocks':[{ "stocks_inserted" : stocksSize - modifiedCount},{ "stocks_modified" : modifiedCount}]}), 200)
